177b489bc8abb7eb72e63b323056edcca533bf4b
clmns <clmns@clmnsui-iMac.local>
2021-07-08 17:27:10 +0900

    not finish

--- routes/nstorage/nstorageRouter.js
+++ routes/nstorage/nstorageRouter.js
@@ -17,17 +17,25 @@ const s3 = new AWS.S3({
   },
 });
 
-let upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.NAVER_CLOUD_BUCKET_NAME,
-    key: function (req, file, cb) {
-      let extension = path.extname(file.originalname);
-      cb(null, Date.now().toString() + extension);
-    },
-    acl: "public-read",
-  }),
+// let upload = multer({
+//   storage: multerS3({
+//     s3: s3,
+//     bucket: process.env.NAVER_CLOUD_BUCKET_NAME,
+//     key: function (req, file, cb) {
+//       let extension = path.extname(file.originalname);
+//       cb(null, Date.now().toString() + extension);
+//     },
+//     acl: "public-read",
+//   }),
+// });
+
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
 });
+
+const upload = multer({ storage: storage }).single("single");
 
 router.get("", (req, res) => {
   res.render("nstorage/nstorage_main", {
@@ -39,9 +47,39 @@ router.get("/upload/single", (req, res) => {
   res.render("nstorage/nstorage_single");
 });
 
-router.post("/upload/single", upload.single("single"), (req, res) => {
-  let imgFile = req.file;
-  res.json(imgFile);
+router.post("/upload/single", upload, (req, res) => {
+  const file = req.file;
+  let fileName = file.originalname.split(".");
+  let fileType = fileName[fileName.length - 1];
+  let fileMimeType = file.mimetype;
+  let labNum = fileName[0].split("-")[3];
+  let filePath =
+    process.env.NAVER_CLOUD_BUCKET_NAME +
+    "/" +
+    labNum +
+    "/" +
+    commonFc.day +
+    "/";
+
+  if (file.mimetype.startsWith("image")) {
+    const params = {
+      Bucket: filePath + "image",
+      Body: file.buffer,
+      ACL: "public-read-write",
+      Key: fileName[0] + "." + fileType,
+    };
+    try {
+      return putObject();
+    } catch (err) {
+      return new ResponseMessage().error(400, err).build();
+    }
+  }
+  console.log(`filePath :: ${filePath}`);
 });
+
+// router.post("/upload/single", upload.single("single"), (req, res) => {
+//   let imgFile = req.file;
+//   res.json(imgFile);
+// });
 
 module.exports = router;
